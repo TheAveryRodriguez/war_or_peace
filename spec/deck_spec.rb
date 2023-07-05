2eb1b981dec48ec41bc2b8e3ac39c9c348e732fc
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-04 20:31:04 -0500

    Update the deck_space.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -11,24 +11,7 @@ card3 = Card.new(:heart, 'Ace', 14)
 cards = [card1, card2, card3]
 deck = Deck.new(cards)
 
-# Test cases
-puts "Cards: #{deck.cards}"
-puts "Rank of card at index 0: #{deck.rank_of_card_at(0)}"
-puts "Rank of card at index 2: #{deck.rank_of_card_at(2)}"
-puts "Cards after rank_of_card_at: #{deck.cards}"
-puts "High ranking cards: #{deck.high_ranking_cards}"
-puts "Percentage of high ranking cards: #{deck.percent_high_ranking}%"
-
-puts "Removing top card..."
-removed_card = deck.remove_card
-puts "Removed card: #{removed_card}"
-puts "Cards after removal: #{deck.cards}"
-puts "High ranking cards after removal: #{deck.high_ranking_cards}"
-puts "Percentage of high ranking cards after removal: #{deck.percent_high_ranking}%"
-
-card4 = Card.new(:club, '5', 5)
-puts "Adding a new card: #{card4}"
-deck.add_card(card4)
-puts "Cards after adding a new card: #{deck.cards}"
-puts "High ranking cards after addition: #{deck.high_ranking_cards}"
-puts "Percentage of high ranking cards after addition: #{deck.percent_high_ranking}%"
+# Tests
+#removed_card = deck.remove_card
+#card4 = Card.new(:club, '5', 5)
+#deck.add_card(card4)
