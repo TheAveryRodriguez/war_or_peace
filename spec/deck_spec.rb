406032918194cc096a57230445529395e7187fa3
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-04 21:13:21 -0500

    change the requrie

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,13 +1,10 @@
 require 'rspec'
 require './lib/card.rb'
-require './lib/deck.rb' 
 
-# Create cards
 card1 = Card.new(:diamond, 'Queen', 12)
 card2 = Card.new(:spade, '3', 3)
 card3 = Card.new(:heart, 'Ace', 14)
 
-# Create deck
 cards = [card1, card2, card3]
 deck = Deck.new(cards)
 
