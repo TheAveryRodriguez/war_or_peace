be35cd93153dc78e21b21eb5b876b6b4fe1e5f2c
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-06 11:06:32 -0500

    changes to the deck_spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -11,14 +11,19 @@ RSpec.describe Deck do
     cards = [card1, card2, card3]
 
     deck = Deck.new(cards)
-    # deck = Deck.new
 
     expect(deck).to be_an_instance_of(Deck)
   end
 
   it "has readable attributes" do
-    deck = Deck.new
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
 
-    expect(deck.cards).to eq([card1, card2, card3])
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq(cards)
   end
 end
